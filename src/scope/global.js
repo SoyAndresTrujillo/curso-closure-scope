172d7dda91592c0889a256ba027c9d5c6e7e2979
SoyAndresTrujillo <andymania.1301@gmail.com>
2023-02-02 15:34:59 -0500

    variables

---
+++ src/scope/global.js
@@ -0,0 +1,25 @@
+// Variables
+
+var a; // declaración
+var b = 1; // declaración y asignamos
+a = 1; // reasignación
+
+var a = 1; // redeclaración
+
+// Global 
+var fruit = 'Banana';
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+console.log(fruit);
+
+function countries() {
+    country = 'Colombia';
+    console.log(country);
+}
+
+countries();
+console.log(country);
