876f0933fd0dc79c4dc42715fe5c78d124b143e7
SoyAndresTrujillo <andymania.1301@gmail.com>
2023-02-02 21:03:36 -0500

    new folder - closure

---
+++ src/closure/lexical.js
@@ -0,0 +1,22 @@
+const myGlobal = 1;
+
+function myFunctionFather() {
+    const myVariableFather = 2;
+
+    console.log(myGlobal);
+
+    function child(sds) {
+        const myVariableChild = 3;
+
+        console.log(myGlobal, myVariableFather);
+
+        function child2() {
+            console.log(myGlobal, myVariableFather, myVariableChild);
+        }
+
+        return child2();
+    }
+    return child();
+}
+
+myFunctionFather();
