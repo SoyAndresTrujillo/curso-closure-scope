84a3f70eded9a32396e385cf287ff69a5a0f4c79
SoyAndresTrujillo <andymania.1301@gmail.com>
2023-02-02 19:15:43 -0500

    Declarar/asignar y Redeclarar/reasignar

---
+++ src/scope/re-variables.js
@@ -0,0 +1,33 @@
+var firstName; // Undefined
+firstName = 'Andres';
+console.log(firstName);
+
+var secondName = 'David'; // Declaramos y asignamos
+secondName = 'Esperanza'; // reasignamos
+console.log(secondName);
+
+var lastName ='Trujillo'; // Declaramos y asignamos
+var lastName ='Perdomo'; // reasignamos
+console.log(lastName);
+
+// Let
+let firstName = 'Andres'; // Declaramos y asignamos
+firstName = 'Andres'; // reasignamos
+console.log(firstName);
+
+let secondName = 'David'; // Declaramos y asignamos
+let secondName = 'Daniel'; // Declaramos y asignamos
+console.log(secondName);
+
+// const
+// const animal = 'dog'; // Declaramos y asignamos
+animal = 'cat'; // reasignamos
+// const animal = 'lion'; // Declaramos y asignamos
+console.log(firstName);
+
+const vehicle = [];
+vehicle.push('🍔');
+console.log(vehicle);
+
+vehicle.pop();
+console.log(vehicle);
